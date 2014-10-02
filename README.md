435a778d08d3b46482311a9f05882e659c7190fb
gr33k01 <nate.hobi@gmail.com>
2014-10-02 15:21:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Good-Slideshow
+
+ Cycle through a good slideshow with Good-Slideshow, a basic script for image slideshows with CSS3 transitions!
+
+## Requirements
+
+* [jQuery](http://jquery.com/)
+
+## HTML structure
+
+```html
+<ul data-slideshow>
+	<li><img src="img/1.jpg"></li>
+	<li><img src="img/2.jpg"></li>
+	<li><img src="img/3.jpg"></li>
+</ul>
+```
+
+## Getting started
+
+* `git clone` into project or download as .zip and copy files into project.
+* Link script (after jQuery) and stylesheet in the head.
+
+```html
+<link href="path/to/goodSlideshow.css" type="text/css" rel="stylesheet" />
+<script src="path/to/jQuery.js" type="text/javascript"></script>
+...
+<script src="path/to/goodSlideshow.js" type="text/javascript"></script>
+```
+
+* Call init method when the DOM is ready.
+
+```javascript
+goodSlideshow.init();
+```
---
+++ goodSlideshow.js
@@ -0,0 +1,97 @@
+/* goodSlideshow.js v1 by @gr33k01 */
+
+var goodSlideshow = {
+
+    config: {
+        selector: '[data-slideshow]',
+        nextBtnText: 'next',
+        prevBtnText: 'prev',
+        animate: true,
+        timingFunction: 'ease',
+        animationDuration: 300, // In milliseconds
+        disableDuration: 0,
+        cssVendorPrefix: ''
+    },
+
+    animationActive: false,
+
+    init: function(config) {
+        var prevBtn = '<li data-prev>' + goodSlideshow.config.prevBtnText + '</li>';
+        var nextBtn = '<li data-next>' + goodSlideshow.config.nextBtnText + '</li>';
+        var navigation = '<ul class="ss-navigation">' + '\n' + prevBtn + '\n' + nextBtn + '\n' + '</ul>';
+
+        // Allow overriding the default settings
+        $.extend(goodSlideshow.config, config);
+        $(navigation).insertAfter(goodSlideshow.config.selector);
+        $('[data-next], [data-prev]').on('click', goodSlideshow.cycle);
+
+        if (goodSlideshow.config.animate) goodSlideshow.setCssTransitions(); 
+    },
+
+    cycle: function(e) {
+
+        var target = e.currentTarget.parentNode.previousElementSibling;
+
+        if (goodSlideshow.animationActive) return;
+
+        if(e.currentTarget.textContent == goodSlideshow.config.prevBtnText){
+            goodSlideshow.previous(target);
+            return;
+        }
+
+        goodSlideshow.next(target, function() {
+            var appendThis = $(target.children[1]).detach();
+            $(target).append(appendThis);
+            goodSlideshow.animationActive = false;
+        });
+    },
+
+    setCssTransitions: function(){
+        var transition = 'visibility 0ms' + ' ' 
+                        + goodSlideshow.config.timingFunction + ' ' 
+                        + goodSlideshow.config.animationDuration + 'ms, opacity ' 
+                        + goodSlideshow.config.animationDuration + 'ms ' 
+                        + goodSlideshow.config.timingFunction + ';';
+
+        var style = '<style>\n' + 'ul' 
+                        + goodSlideshow.config.selector + ' li {\n\t' 
+                        + goodSlideshow.config.cssVendorPrefix + 'transition: ' 
+                        + transition + '\n}\n</style>';
+
+        goodSlideshow.config.cssVendorPrefix = (function() {
+
+            var computedStyles = window.getComputedStyle(document.documentElement, '');
+            var prefix = (Array.prototype.slice
+                .call(computedStyles)
+                .join('')
+                .match(/-(moz|webkit|ms)-/))[1];
+
+            return '-' + prefix + '-';
+        }());
+
+        $('head').append(style);
+
+        // Diable for 10ms longer than the animation duration
+        goodSlideshow.config.disableDuration = goodSlideshow.config.animationDuration + 10;
+    },
+
+    previous: function(activeTarget) {
+
+        var index = activeTarget.children.length - 1;
+        var target = $(activeTarget.children[index]).detach();
+
+        $(activeTarget).prepend(target);
+        goodSlideshow.animationActive = true;
+        window.setTimeout(function() {
+            goodSlideshow.animationActive = false;
+        }, goodSlideshow.config.disableDuration);
+    },
+
+    next: function(activeTarget, callback) {
+        var target = $(activeTarget.children[1]).detach();
+
+        $(activeTarget).prepend(target);
+        goodSlideshow.animationActive = true;
+        window.setTimeout(callback, goodSlideshow.config.disableDuration);
+    }
+}
